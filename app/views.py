70c30b0231896546d5f326dadc4ffe8e640b7cbc
Ruhshan <ruhshan.ahmed@gmail.com>
2021-03-19 18:55:10 +0600

    Initialize with necessary files

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.http import JsonResponse
+import datetime
+
+# Create your views here.
+def current_date(request):
+    return JsonResponse({"date": datetime.datetime.now().astimezone().strftime("%a %b %d %I:%M:%S %Z %Y")})
