1810c7a259864cde14cc3cfaaa8893e33ce328bf
Ruhshan <ruhshan.ahmed@gmail.com>
2021-03-19 20:04:28 +0600

    Remove unnecesarry print statement

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 import datetime
 
+
 # Create your views here.
 def current_date(request):
-    print("Received request")
     return JsonResponse({"date": datetime.datetime.now().astimezone().strftime("%a %b %d %I:%M:%S %Z %Y")})
