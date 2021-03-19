516cffbb23a2559377d1ff5d97311004b0c714ff
Ruhshan <ruhshan.ahmed@gmail.com>
2021-03-19 19:49:34 +0600

    Add print statement for testing automated build

--- app/views.py
+++ app/views.py
@@ -3,4 +3,5 @@ import datetime
 
 # Create your views here.
 def current_date(request):
+    print("Received request")
     return JsonResponse({"date": datetime.datetime.now().astimezone().strftime("%a %b %d %I:%M:%S %Z %Y")})
